ffbce0943d9f7c0405313affc5ad2dcebec02129
scott <scott@scott-ThinkPad-T430>
2021-10-02 07:14:55 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -2,4 +2,8 @@ from unittest import TestCase
 import dodoku.create as create 
 
 class CreateTest(TestCase):
-        pass
+    def test_Create_010_ShouldReturn3Keys(self):
+        expectedResult = {'grid':'', 'integrity':'', 'status':''}
+        parms = {'op':'create','level':'1'}
+        actualResult = create._create(parms)
+        self.assertEqual(expectedResult.keys(), actualResult.keys())
