68f4dbf6709b7b76a0e35b82761ae7d04b3ca33f
scott <scott@scott-ThinkPad-T430>
2021-10-02 12:50:38 -0500

    Red Light |

--- dodoku/test/createTest.py
+++ dodoku/test/createTest.py
@@ -7,3 +7,17 @@ class CreateTest(TestCase):
         parms = {'op':'create','level':'1'}
         actualResult = create._create(parms)
         self.assertEqual(expectedResult.keys(), actualResult.keys())
+        
+    def test_Create_020_NominalLevelValue(self):
+        gridValue= [0,-6,0,0,0,0,0,-5,-9,-9,-3,0,-4,-8,0,0,0,0,0,0,0,0,0,
+                                  -7,-3,0,0,0,-5,0,0,-1,0,0,-4,-6,0,0,0,0,0,-6,0,-9,0,0,
+                                  -8,-1,-2,0,0,0,0,0,0,0,0,0,-7,0,0,0,0,-8,0,-4,0,0,-1,0,0,0,0,
+                                  -9,0,0,0,0,0,0,0,0,0,0,0,-7,0,-5,-8,0,0,0,-2,0,0,0,-6,-1,0]
+        hashValue = '6fcd71ef7722e7573d2f607a35cfa48f72b03c4cea135ac31f7ef73a58e50a8a'
+        statusValue = 'ok'
+        parms = {'op':'create', 'level':'2'}
+        result = create._create(parms)
+        self.assertEqual(result.grid, gridValue)
+        self.assertIn(result.integrity, hashValue)
+        self.assertEqual(result.status, statusValue)
+        
