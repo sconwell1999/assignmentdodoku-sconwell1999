947312abac8b0c32d8ce25a220f032bff1f69f0e
scott <scott@scott-ThinkPad-T430>
2021-10-24 11:35:35 -0500

    Green Light |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -13,6 +13,8 @@ def _insert(parms):
         result['grid'][(row-1)*9 + (column - 1)] = int(parms['value'])
     elif row < 10 and column < 16:
         result['grid'][54 + (row-7)*15 + (column - 1)] = int(parms['value'])
+    elif row < 16 and column < 16:
+        result['grid'][99 + (row-10)*9 + (column - 7)] = int(parms['value'])
     r = random.randint(0,56)
     result['integrity'] = getHash(result['grid'])[r:r+8]
     result['status'] = 'ok'
