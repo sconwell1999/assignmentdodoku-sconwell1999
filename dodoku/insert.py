223a2a10a94048e17f2e9d838540ecfbfd360f88
scott <scott@scott-ThinkPad-T430>
2021-10-25 16:31:45 -0500

    Refactor |

--- dodoku/insert.py
+++ dodoku/insert.py
@@ -69,19 +69,22 @@ def columnValuesCheck(grid, column):
         for x in range(0,48,9):
             values.append(grid[x+column-1])
     elif column in range(7,10):
-        for x in range(0,48,9):
-            values.append(grid[x+column-1])
-        for x in range(54,85,15):
-            values.append(grid[x+column-1])
+        for x in range(6,52,9):
+            values.append(grid[x+column-7])
+        for x in range(60,91,15):
+            values.append(grid[x+column-7])
         for x in range(99,145,9):
             values.append(grid[x+column-7])
     elif column in range(10,16):
-        for x in range(63,148,9):
+        for x in range(63,94,15):
             values.append(grid[x+column-10])
-            
+        for x in range(99,148,9):
+            values.append(grid[x+column-10])
+    
+    values = [abs(x) for x in values]        
     if len(values) == 15:        
         val1 = values[0:9]
-        val2 = values[6:16]
+        val2 = values[6:15]
         #remove 0's
         val1 = list(filter(lambda val: val !=  0, val1))
         val2 = list(filter(lambda val: val !=  0, val2))
@@ -100,15 +103,15 @@ def rowValuesCheck(grid, row):
     elif row in range(10,16):
         for x in range(0,9):
             values.append(grid[99+(row-10)*9+x])
-            
+    
+    values = [abs(x) for x in values]        
     if len(values) == 15:
         val1 = values[0:9]
-        val2 = values[6:16]
+        val2 = values[6:15]
         #remove 0's
         val1 = list(filter(lambda val: val !=  0, val1))
         val2 = list(filter(lambda val: val !=  0, val2))
         return len(val1) == len(set(val1)) and len(val2) == len(set(val2))
-    
     values = list(filter(lambda val: val !=  0, values)) 
     return len(values) == len(set(values))
 
@@ -123,12 +126,13 @@ def subGridValuesCheck(grid, row, column):
     if row in range(7,10):
         for i in range(3):
             for j in range(3):
-                values.append(grid[(startRow-1+i)*15+startColumn+j])
+                values.append(grid[54+(startRow-7+i)*15+startColumn+j])
     if row in range(10,16):
         for i in range(3):
             for j in range(3):
-                values.append(grid[(startRow-1+i)*9+startColumn+j])
+                values.append(grid[99+(startRow-10+i)*9+startColumn+j])
                 
     values = list(filter(lambda val: val !=  0, values))
+    values = [abs(x) for x in values]
     return len(values) == len(set(values))
     
